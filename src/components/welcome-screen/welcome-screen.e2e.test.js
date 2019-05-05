190aeee3c49c5571418a8cce00c5b847537c919c
Николай <arcmag-work@mail.ru>
2019-05-05 16:30:19 +0700

    Выполняет шаги из задания - Учебный проект: под покровом тестов

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WelcomeScreen from './welcome-screen.jsx';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe(`e2e test WelcomeScreen`, () => {
+  it(`Test button start click`, () => {
+    const startGameFn = jest.fn();
+    const tree = shallow(<WelcomeScreen
+      time={0}
+      errorCount={0}
+      onButtonStartClick={startGameFn}
+    />);
+    const btnStart = tree.find(`.welcome__button`);
+    expect(startGameFn).toHaveBeenCalledTimes(0);
+    btnStart.simulate(`click`);
+    expect(startGameFn).toHaveBeenCalledTimes(1);
+  });
+});
